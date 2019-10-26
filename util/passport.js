69ee8e09d87d9004acbbd521722aaa5435d06c54
Marco Ma <marcoma2013@outlook.com>
2019-10-26 23:45:05 +0800

    added passport.js and .gitignore file

---
+++ util/passport.js
@@ -0,0 +1,49 @@
+let passport = require('passport');
+let localStategy = require('passport-local').Strategy;
+let githubStrategy = require('passport-github').Strategy;
+let User = require('../model/user');
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+passport.use('local', new localStategy({
+    usernameField: 'userName',
+    passwordField: 'password'
+}, async (userName, password, done) => {
+    try {
+        const user = await User.findOne({
+            'username': userName,
+            'password': password
+        });
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false, {
+                message: 'User name or password is wrong!'
+            });
+        }
+    } catch (err) {
+        return done(err)
+    }
+}));
+
+passport.use(new githubStrategy({
+    clientID: '',
+    clientSecret: '',
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback",
+    passReqToCallback: true
+}, (req, accessToken, refreshToken, profile, done) => {
+    process.nextTick(function () {
+        // To keep the example simple, the user's GitHub profile is returned to
+        // represent the logged-in user.  In a typical application, you would want
+        // to associate the GitHub account with a user record in your database,
+        // and return that user instead.
+        return done(null, profile);
+    });
+}
+));
